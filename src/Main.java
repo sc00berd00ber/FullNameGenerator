a387c97cb082e2487e45beff89068070a1a5dcb2
Nat Tessema <natnaeltessema1@gmail.com>
2025-04-15 11:14:48 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first name: ");
+        String firstName = input.nextLine();
+
+        System.out.println("Enter your middle name initial: ");
+        String middleName = input.nextLine();
+
+        System.out.println("Enter your last name: ");
+        String lastName = input.nextLine();
+
+        System.out.println("Enter your suffix: ");
+        String suffix = input.nextLine();
+
+        StringBuilder fullName = new StringBuilder();
+        fullName.append(firstName);
+
+        if (!middleName.isEmpty()) {
+            fullName.append(" ").append(middleName);
         }
+
+        fullName.append(" ").append(lastName);
+
+        if (!suffix.isEmpty()) {
+            fullName.append(", ").append(suffix);
+        }
+
+        System.out.println("Full Name: " + fullName.toString());
     }
 }
